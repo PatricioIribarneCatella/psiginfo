ba4ce1d60eaf6e95284b5b10545ad44de722fe3d
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2018-04-24 23:21:41 -0300

    sscanf passed to fscanf

--- psiginfo.c
+++ psiginfo.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
 
 #define BUFLEN 128
 
@@ -15,13 +11,13 @@ static void print_signal_status(const char* title, const char* sig_bitmask) {
 	printf("\t%s\n", sig_bitmask);
 }
 
-static void find_signal_info(const char* file) {
+static void find_signal_info(FILE* file) {
 
 	static char blk_sigs[BUFLEN];
 	static char ign_sigs[BUFLEN];
 	static char cgt_sigs[BUFLEN];
 
-	sscanf(file, "SigBlk: %s\nSigIgn: %s\nSigCgt: %s\n",
+	fscanf(file, "SigBlk: %s\nSigIgn: %s\nSigCgt: %s\n",
 		blk_sigs, ign_sigs, cgt_sigs);
 
 	print_signal_status("Blocked Signals", blk_sigs);
@@ -31,41 +27,19 @@ static void find_signal_info(const char* file) {
 
 static void psiginfo(int pid) {
 
-	int fd;
-	char* file;
-	size_t len;
-	struct stat sb;
+	FILE* f;
 	char buf[BUFLEN] = {0};
 
 	snprintf(buf, sizeof buf, "/proc/%d/status", pid);
 
-	if ((fd = open(buf, O_RDONLY, 0)) < 0) {
+	if (!(f = fopen(buf, "r"))) {
 		perror("cannot open file ");
 		_exit(EXIT_FAILURE);
 	}
-
-	if (fstat(fd, &sb) < 0) {
-		perror("cannot stat file ");
-		close(fd);	
-		_exit(EXIT_FAILURE);
-	}
-
-	len = sb.st_size & ~(sysconf(_SC_PAGE_SIZE) - 1);
-
-	if ((file = mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
-		perror("cannot map file ");
-		close(fd);
-		_exit(EXIT_FAILURE);
-	}
-
-	close(fd);
 	
-	find_signal_info(file);
+	find_signal_info(f);
 
-	if (munmap(file, sb.st_size) < 0) {
-		perror("cannot unmap ");
-		_exit(EXIT_FAILURE);
-	}
+	fclose(f);
 }
 
 int main(int arg, char const* argv[]) {
